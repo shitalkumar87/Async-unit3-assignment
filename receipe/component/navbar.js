ce435d8a867d7a0c99870bbb7108f4d8cb4371a0
shitalkumar87 <101393465+shitalkumar87@users.noreply.github.com>
2022-07-16 15:24:42 +0530

    receipe login

---
+++ receipe/component/navbar.js
@@ -0,0 +1,33 @@
+function navbar(){
+    return       ` <h3><a href="./home.html">Home</a></h3>   
+
+<div class="options">
+    
+<h3><a href="./randomsearch.html">Search Receipe</a></h3>
+<h3> <a href="./random.html">Random Meal</a></h3>
+<h3> <a id="login"  href="./login.html">Login</a></h3>
+<h3> <a   href="./signin.html">Signup</a></h3>
+    
+     
+</div>`;
+
+}
+
+function topbar(){
+    return       ` <h3><a href="./home.html">Home</a></h3>   
+
+<div class="options">
+    
+<h3><a href="./randomsearch.html">Search Receipe</a></h3>
+<h3> <a href="./random.html">Random Meal</a></h3>
+<h3> <a id="login"   >Login</a></h3>
+ 
+    
+     
+</div>`;
+
+}
+
+ export {navbar,topbar}
+  
+  
